cf6767537d8a2e18fd5515c01a30d1c9bc91b1e5
shuuhei miwa <anonymity.syu@gmail.com>
2016-11-11 11:09:34 +0000

    Implementention of Edit&Delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root 'messages#index'
   resources :messages , except: [:index, :new]
 
-#  resources :messages , only: [:create, :index]
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
